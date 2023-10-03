e017335f5f06857f15b50fee027caf7f3310a32c
HENDRICE_Rémy <r.hendrice@student.helmo.be>
2023-10-03 17:27:03 +0200

    Ajout note (refactor accept dans classe abstraite)

--- Cas2/src/components/Component.java
+++ Cas2/src/components/Component.java
@@ -5,5 +5,7 @@ import visitor.ComponentVisitor;
 public interface Component {
     double getPrice();
     boolean validateConfiguration();
+    //On pourrait refactor accept dans une classe abstraite pour éviter de la redéfinir à chaque fois, 
+    // mais ferait référence à classe abstraite, pas classe concrète
     String accept(ComponentVisitor visitor);
 }
